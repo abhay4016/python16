646fef5b5e1b01c98682439d99eec7c303489ca4
abhay4016 <ygg2551@proton.me>
2026-09-21 15:57:05 +0530

    Update and rename temperature.py to Temperature.py

--- Temperature.py
+++ Temperature.py
@@ -1,9 +1,9 @@
 def celsius_to_fahrenheit(celsius):
-    fahrenheit = (celsius * 9/5) + 32 # formula to convert celsius to fahrenheit
+    fahrenheit = (celsius * 9/5) + 32
     return fahrenheit
 
 celsius = float(input("Enter temperature in Celsius: "))
-fahrenheit = celsius_to_fahrenheit(celsius) # calling the function
+fahrenheit = celsius_to_fahrenheit(celsius) 
 print(f"{celsius}°C is equal to {fahrenheit}°F")
 
 
