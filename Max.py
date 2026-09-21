e6ddfccef2f68c127d3915af1feba6a6c97fc0ce
abhay4016 <ygg2551@proton.me>
2026-09-21 15:55:57 +0530

    Update and rename max.py to Max.py

--- Max.py
+++ Max.py
@@ -1,16 +1,16 @@
-# 4. write a function to find the max of two numbers taking input from the user
+
 def find_max():
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
-    return max(num1, num2) # using the inuilt max() function to find the maximum of the two numbers
+    return max(num1, num2) 
 
 
-print("The maximum of the two numbers is:", find_max()) # calling the function and print the result
+print("The maximum of the two numbers is:", find_max()) 
 
 
 
 
-# now withput using the inbuilt max() function
+
 
 def find_max_without_max():
     num1 = float(input("Enter the first number: "))
