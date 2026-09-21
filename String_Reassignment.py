b0c8e8c335da16a42a9912794b768a3571884eae
abhay4016 <ygg2551@proton.me>
2026-09-21 15:56:42 +0530

    Update and rename string_reassignment.py to String_Reassignment.py

---
+++ String_Reassignment.py
@@ -0,0 +1,16 @@
+
+
+
+def change_string(s):
+    if s:=
+        s = "x" + s[1:] =
+        
+    return s
+
+
+
+
+original_string = "hello"
+print("Original string:", original_string)
+new_string = change_string(original_string)=
+print("New string:", new_string)
